a8df8c36cd86398617bd07d02a3f0f4be2b59386
Timo Paulssen <timonator@perpetuum-immobile.de>
2012-05-19 02:35:33 +0200

    added a short setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qargparse",
+    version="0.0.1",
+    description="automatically generate QDialogs from argparse objects",
+    packages = find_packages(),
+    )
+
